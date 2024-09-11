d324a419024372a165f775575d16b8f886d7f2d8
Matsuda1129 <hicotaku@gmail.com>
2024-09-11 22:48:32 +0900

    db設定

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
 func main() {
+	dsn := "host=db user=postgres password=password dbname=db  port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&User{})
+	fmt.Println("migrated")
+
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
 
